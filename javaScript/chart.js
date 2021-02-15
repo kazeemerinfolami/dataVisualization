851a1c42bcef650c6002cf235cc73087e0a0162b
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-02-15 01:35:59 +0100

    Made some changes on the styles and Chart.js

--- javaScript/chart.js
+++ javaScript/chart.js
@@ -1,10 +1,10 @@
 //pieChart
 
 //columns
-let labels1 = ["YES", "YES BUT IN GREEN", "maybe", "but", "why"]
+let labels1 = ["yes", "no", "prefer_not_to_say"]
 //data collected
-let data1 = [31, 4, 3, 10, 70]
-let tableName = "Aspora Recorgnition"
+let data1 = [521, 459, 20]
+let tableName = "Have you ever heard about Aspora ?"
 let colors1 = ["#F05837", "blue", "green", "#F3e93b", "#68975a6"]
 let myChart1 = document.getElementById("doughnutChart").getContext("2d")
 let chart1 = new Chart(myChart1, {
@@ -27,10 +27,10 @@ let chart1 = new Chart(myChart1, {
 //Bar
 
 //columns
-let labels2 = ["YES", "YES BUT IN GREEN", "maybe", "but", "why"]
+let labels2 = ["yes", "no", "prefer_not_to_say"]
 //data collected
-let data2 = [32, 4, 2, 20, 70]
-let tableName2 = "Aspora Recorgnition"
+let data2 = [456, 417, 127]
+let tableName2 = "Do you currently play any kind of sport?"
 let colors2 = ["#72a2c0", "#93a806", "green", "#a4978e", "#be9063"]
 let myChart2 = document.getElementById("barChart").getContext("2d")
 let chart2 = new Chart(myChart2, {
@@ -53,52 +53,90 @@ let chart2 = new Chart(myChart2, {
     }
 })
 
-//radar
+
+//Bar
 
 //columns
-let labels3 = ["agree", "disagree", "yes", "no"]
-let tableName3 = "Aspora Recorgnition"
-let myChart3 = document.getElementById("radarChart").getContext("2d")
+let labels3 = ["laced", "laceless", "for_both"]
+//data collected
+let data3 = [420, 234, 127]
+let tableName3 = "Do you want these shoes to be laced/laceless?"
+let colors3 = ["green", "#a4978e", "#be9063"]
+let myChart3 = document.getElementById("barChart2").getContext("2d")
 let chart3 = new Chart(myChart3, {
-    type: "radar",
+    type: "bar",
     data: {
         labels: labels3,
         datasets: [{
-            label: "female",
-            fill: true,
-            backgroundColor: "rgba(179,181,198,0.2)",
-            borderColor: "rgba(179,181,198,1)",
-            pointBorderColor: "#fff",
-            pointBackgroundColor: "rgba(179,181,198,0.2)",
-            data: [3, 10, 10, 28]
-        },
-        {
-            label: "male",
-            fill: true,
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            pointBorderColor: "#fff",
-            pointBackgroundColor: "rgba(255,99,132,1)",
-            data: [4, 20, 9, 18]
+            data: data3,
+            backgroundColor: colors3
         }]
     },
     options: {
         title: {
             text: tableName3,
             display: true
+        },
+        legend: {
+            display: false
         }
     }
 })
+//radar
+
+//columns
+// let labels3 = ["laced", "laceless", "for_both"]
+// let tableName3 = "Do you want these shoes to be laced/laceless"
+// let myChart3 = document.getElementById("radarChart").getContext("2d")
+// let chart3 = new Chart(myChart3, {
+//     type: "radar",
+//     data: {
+//         labels: labels3,
+//         datasets: [{
+//             label: "laced",
+//             fill: true,
+//             backgroundColor: "rgba(179,181,198,0.2)",
+//             borderColor: "rgba(179,181,198,1)",
+//             pointBorderColor: "#fff",
+//             pointBackgroundColor: "rgba(179,181,198,0.2)",
+//             data: [234]
+//         },
+//         {
+//             label: "laceless",
+//             fill: true,
+//             backgroundColor: "rgba(255,99,132,0.2)",
+//             borderColor: "rgba(255,99,132,1)",
+//             pointBorderColor: "#fff",
+//                 pointBackgroundColor: "rgba(255,99,132,0.2)",
+//                 data: [420]
+//             },
+//             {
+//                 label: "for_both",
+//                 fill: true,
+//                 backgroundColor: "black",
+//                 borderColor: "grey",
+//                 pointBorderColor: "#fff",
+//                 pointBackgroundColor: "yellow",
+//                 data: [417]
+//             }]
+//     },
+//     options: {
+//         title: {
+//             text: tableName3,
+//             display: true
+//         }
+//     }
+// })
 
 
 //pieChart
 
 //columns
-let labels4 = ["YES", "YES BUT IN GREEN", "maybe", "but", "why"]
+let labels4 = ["38_40", "41_43", "44_46", "47_and_above"]
 //data collected
-let data4 = [34, 4, 3, 40, 70]
-let tableName4 = "Aspora Recorgnition"
-let colors4 = ["#72a2c0", "#93a806", "green", "#a4978e", "#be9063"]
+let data4 = [138, 262, 370, 237]
+let tableName4 = "What shoe size range would you personally like for the shoe to come in?"
+let colors4 = ["#72a2c0", "#93a806", "#a4978e", "#be9063"]
 let myChart4 = document.getElementById("pieChart").getContext("2d")
 let chart4 = new Chart(myChart4, {
     type: "pie",
@@ -120,10 +158,10 @@ let chart4 = new Chart(myChart4, {
 //line
 
 //columns
-let labels5 = ["YES", "YES BUT IN GREEN", "maybe", "but", "why"]
+let labels5 = ["2000_5000", "6000_10000", "11000_15000", "16000_20000", "21000_and_more"]
 //data collected
-let data5 = [35, 4, 5, 50, 70]
-let tableName5 = "Aspora Recorgnition"
+let data5 = [189, 307, 295, 167, 50]
+let tableName5 = "What price range do you believe that these shoes will cost upon release?"
 let colors5 = ["red", "blue", "green", "yellow", "gold"]
 let myChart5 = document.getElementById("lineChart").getContext("2d")
 let chart5 = new Chart(myChart5, {
@@ -142,6 +180,32 @@ let chart5 = new Chart(myChart5, {
         },
         legend: {
             display: false
+        }
+    }
+})
+
+//pieChart
+
+//columns
+let labels6 = ["2000_5000", "6000_10000", "11000_15000", "16000_20000", "21000_and_more"]
+//data collected
+let data6 = [189, 307, 295, 167, 50]
+let tableName6 = "What price range do you believe that these shoes will cost upon release?"
+let colors6 = ["#F05837", "blue", "green", "#F3e93b", "#68975a6"]
+let myChart6 = document.getElementById("doughnutChart2").getContext("2d")
+let chart6 = new Chart(myChart6, {
+    type: "doughnut",
+    data: {
+        labels: labels6,
+        datasets: [{
+            data: data6,
+            backgroundColor: colors6
+        }]
+    },
+    options: {
+        title: {
+            text: tableName6,
+            display: true
         }
     }
 })
